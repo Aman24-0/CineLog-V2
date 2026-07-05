ebafc0ceaf4cbbaf15b0b9029bf2b858cb9f5ec1
Aman Dahayat <89609354+Aman24-0@users.noreply.github.com>
2026-07-05 16:00:45 +0530

    feat(app): create application shell

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,19 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
+
 import "./app/globals.css";
+import AppShell from "./app/AppShell";
 
 export default function App() {
   return (
     <Router
       root={(props) => (
-        <Suspense fallback={<div>Loading...</div>}>
-          {props.children}
-        </Suspense>
+        <AppShell>
+          <Suspense fallback={<div>Loading...</div>}>
+            {props.children}
+          </Suspense>
+        </AppShell>
       )}
     >
       <FileRoutes />
