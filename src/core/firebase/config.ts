99094a9f70dc3c331d3c857014b3a9d4641dcdea
Aman Dahayat <89609354+Aman24-0@users.noreply.github.com>
2026-07-05 15:56:44 +0530

    Update config.tsfeat(firebase): migrate authentication layer

--- src/core/firebase/config.ts
+++ src/core/firebase/config.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,7 +11,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
