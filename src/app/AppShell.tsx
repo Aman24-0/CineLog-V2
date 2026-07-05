d4875bfb0dd414edd37c66660893f84f35f9963a
Aman Dahayat <89609354+Aman24-0@users.noreply.github.com>
2026-07-05 16:05:39 +0530

    feat(ui): migrate toast system

--- src/app/AppShell.tsx
+++ src/app/AppShell.tsx
@@ -1,9 +1,12 @@
 import { ParentComponent } from "solid-js";
+import ToastContainer from "~/shared/ui/ToastContainer";
 
 const AppShell: ParentComponent = (props) => {
   return (
     <div class="min-h-screen bg-black text-white">
       {props.children}
+
+      <ToastContainer />
     </div>
   );
 };
