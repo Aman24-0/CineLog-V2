aa61373a50f607e0c217cd4331fa16784ae8cba2
Aman Dahayat <89609354+Aman24-0@users.noreply.github.com>
2026-07-05 16:12:03 +0530

    Update AppShell.tsxfeat(ui): add bottom navigation foundation

--- src/app/AppShell.tsx
+++ src/app/AppShell.tsx
@@ -1,12 +1,15 @@
 import { ParentComponent } from "solid-js";
 import ToastContainer from "~/shared/ui/ToastContainer";
+import BottomNavigation from "~/shared/ui/BottomNavigation";
 
 const AppShell: ParentComponent = (props) => {
   return (
-    <div class="min-h-screen bg-black text-white">
+    <div class="min-h-screen bg-black pb-16 text-white">
       {props.children}
 
       <ToastContainer />
+
+      <BottomNavigation />
     </div>
   );
 };
