4551400af5ccbfa6bd40f51e6f31ed15578d35d3
Aman Dahayat <89609354+Aman24-0@users.noreply.github.com>
2026-07-06 10:13:17 +0530

    Update AppShell.tsx

--- src/app/AppShell.tsx
+++ src/app/AppShell.tsx
@@ -1,8 +1,13 @@
-import { ParentComponent } from "solid-js";
+import { ParentComponent, lazy, Suspense, Show } from "solid-js";
 import ToastContainer from "~/shared/ui/ToastContainer";
 import BottomNavigation from "~/shared/ui/BottomNavigation";
+import { useModalState } from "~/shared/hooks/useModalState";
+
+const DetailsModal = lazy(() => import("~/features/details/DetailsModal"));
 
 const AppShell: ParentComponent = (props) => {
+  const { selectedItem } = useModalState();
+
   return (
     <div class="min-h-screen bg-black pb-16 text-white">
       {props.children}
@@ -10,6 +15,12 @@ const AppShell: ParentComponent = (props) => {
       <ToastContainer />
 
       <BottomNavigation />
+
+      <Show when={selectedItem()}>
+        <Suspense fallback={null}>
+          <DetailsModal />
+        </Suspense>
+      </Show>
     </div>
   );
 };
