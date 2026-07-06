947b956a9e6ffe447d21f7e0f6a94034ca62a85d
Aman Dahayat <89609354+Aman24-0@users.noreply.github.com>
2026-07-06 09:54:19 +0530

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,26 @@
+// src/app.tsx
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 
 import "./app/globals.css";
 import AppShell from "./app/AppShell";
+import { VaultProvider } from "~/features/watchlist/useVault";
 
 export default function App() {
   return (
-    <Router
-      root={(props) => (
-        <AppShell>
-          <Suspense fallback={<div>Loading...</div>}>
-            {props.children}
-          </Suspense>
-        </AppShell>
-      )}
-    >
-      <FileRoutes />
-    </Router>
+    <VaultProvider>
+      <Router
+        root={(props) => (
+          <AppShell>
+            <Suspense fallback={<div>Loading...</div>}>
+              {props.children}
+            </Suspense>
+          </AppShell>
+        )}
+      >
+        <FileRoutes />
+      </Router>
+    </VaultProvider>
   );
 }
